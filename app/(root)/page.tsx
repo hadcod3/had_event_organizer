a6ae5de7a32b6477057879bde6e23864f8386d68
Nur Hadi <decoding0506@gmail.com>
2024-03-08 01:14:08 +0700

    finished

--- app/(root)/page.tsx
+++ app/(root)/page.tsx
@@ -1,17 +1,23 @@
 import { Button } from "@/components/ui/button";
+import { getAllEvents } from "@/lib/actions/event.actions";
+import { SearchParamProps } from '@/types';
 import Image from "next/image";
 import Link from "next/link";
 import styles from "../../styles/home.module.css"
-import { imageList } from "@/constants";
 import Collection from "@/components/shared/Collection";
-import { getAllEvents } from "@/lib/actions/event.actions";
+import Search from "@/components/shared/Search";
+import CategoryFilter from "@/components/shared/CategoryFilter";
 
-export default async function Home() {
+export default async function Home({ searchParams }: SearchParamProps) {
+    const page = Number(searchParams?.page) || 1;
+    const searchText = (searchParams?.query as string) || '';
+    const category = (searchParams?.category as string) || '';
+
     const events = await getAllEvents({
-        query: '',
-        category: '',
-        page: 1,
-        limit: 6
+        query: searchText,
+        category,
+        page,
+        limit: 3
     })
 
     return (
@@ -52,17 +58,17 @@ export default async function Home() {
             <section id="events" className="wrapper my-8 flex flex-col md:gap-12">
                 <h2 className="h2-bold">Trusted by <br/> Thousands of Events</h2>
                 <div className="flex w-full flex-col gap-5 md:flex-row">
-                    Search
-                    CategoryFilter
+                    <Search />
+                    <CategoryFilter/>
                 </div>
                 <Collection
                 data={events?.data}
                 emptyTitle="No Events Found"
                 emptyStateSubtext="Check later"
                 collectionType="All_Events"
-                limit={6}
-                page={1}
-                totalPages={2}
+                limit={3}
+                page={page}
+                totalPages={events?.totalPages}
                 />
             </section>
 
